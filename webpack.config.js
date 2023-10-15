f6110b92867263faac5db1042b7632f14345e441
LanaVol <svolosh4yk@gmail.com>
2023-10-15 20:05:58 +0300

    work with clean-webpack-plugin&update

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  // режим розробки
   mode: "development",
+  // вхідний файл, що вказує з якого місця webpack має почати
   entry: {
     main: "./src/index.js",
     analytics: "./src/analytics.js",
   },
+  // після збирання всі скрипти будуть зібрані і поміщені в даний файл(-и) за даною адресою
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  // плагіни
   plugins: [
     new HTMLWebpackPlugin({
+      // шлях до відповідного файлу html з контентом
       template: "./src/index.html",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
