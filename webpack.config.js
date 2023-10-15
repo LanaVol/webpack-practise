479d86ea91b35bd447cea3ac8413b99589985928
LanaVol <svolosh4yk@gmail.com>
2023-10-15 19:24:41 +0300

    basic min webpack config without clear

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+    analytics: "./src/analytics.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
