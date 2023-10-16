b920a69d436498b209cf480d86a42304f85286c0
LanaVol <svolosh4yk@gmail.com>
2023-10-16 15:34:08 +0300

    work with style&css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  context: path.resolve(__dirname, "src"),
   // режим розробки
   mode: "development",
   // вхідний файл, що вказує з якого місця webpack має почати
   entry: {
-    main: "./src/index.js",
-    analytics: "./src/analytics.js",
+    main: "./index.js",
+    analytics: "./analytics.js",
   },
   // після збирання всі скрипти будуть зібрані і поміщені в даний файл(-и) за даною адресою
   output: {
@@ -19,8 +20,16 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       // шлях до відповідного файлу html з контентом
-      template: "./src/index.html",
+      template: "./index.html",
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
